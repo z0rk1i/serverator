d02fecf638dcf90946cb50f0f7b963a3fe2a8dab
z0rk1i <z0rk1@ya.ru>
2019-04-01 22:44:51 +0300

    Aaand less verbose

--- checker.rb
+++ checker.rb
@@ -8,4 +8,3 @@ doc = Nokogiri::HTML(open('http://testnet2.quarkchain.io/getFullStats'))
 JSON.parse(doc)['shards'].last(4).each do |shard|
   ap "#{shard['fullShardId']} => #{shard['difficulty']}"
 end
-ap JSON.parse(doc)
