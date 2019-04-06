ef3897e0c504ab45d55eae95fd3fec299a73c3a6
z0rk1i <z0rk1@ya.ru>
2019-04-07 00:20:43 +0300

    Turned into loop

--- checker.rb
+++ checker.rb
@@ -3,8 +3,13 @@ require 'open-uri'
 require 'ap'
 require 'json'
 
-doc = Nokogiri::HTML(open('http://testnet2.quarkchain.io/getFullStats'))
-
-JSON.parse(doc)['shards'].last(4).each do |shard|
-  ap "#{shard['fullShardId']} => #{shard['difficulty']}"
+while true do
+  puts Time.now.to_s
+  puts '=' * 26
+  doc = Nokogiri::HTML(open('http://testnet2.quarkchain.io/getFullStats'))
+  JSON.parse(doc)['shards'].last(4).each do |shard|
+    ap "#{shard['fullShardId']} => #{shard['difficulty']}"
+  end
+  puts '-' * 26
+  sleep 60
 end
