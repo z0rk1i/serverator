2da676c32a09d9510cacb0b9fe9aa8dc8ee0a633
Aleksandr <al.kudashkin@gmail.com>
2019-05-01 13:42:07 +0300

    More styling

--- checker.rb
+++ checker.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require 'nokogiri'
 require 'open-uri'
 require 'ap'
@@ -6,7 +8,7 @@ require 'json'
 while true do
   puts Time.now.to_s
   puts '=' * 26
-  doc = Nokogiri::HTML(open('http://testnet2.quarkchain.io/getFullStats'))
+  doc = Nokogiri::HTML(open('http://mainnet.quarkchain.io/getFullStats'))
   JSON.parse(doc)['shards'].last(4).each do |shard|
     ap "#{shard['fullShardId']} => #{shard['difficulty']}"
   end
