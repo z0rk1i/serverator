1f4782be273b66b19a8e3889793ad68a8f5716df
Aleksandr <al.kudashkin@gmail.com>
2019-05-19 22:20:58 +0300

    Lost server

--- scp.rb
+++ scp.rb
@@ -1,6 +1,6 @@
 require 'parallel'
 
-dest = ['root@68.183.191.242', 'root@198.71.61.205']
+dest = ['root@68.183.191.242']
 file = '/home/z0rk1/Загрузки/cminer-0.2.0-beta.3-linux3-amd64/'
 
 Parallel.each(dest, in_processes: dest.size) do |d|
